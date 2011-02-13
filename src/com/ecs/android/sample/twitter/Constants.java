db8d1a2f458067afa18810cfff7aaa15d1516bd1
ddewaele <ddewaele@gmail.com>
2011-02-13 16:45:53 +0100

    initial checkin

--- src/com/ecs/android/sample/twitter/Constants.java
+++ src/com/ecs/android/sample/twitter/Constants.java
@@ -2,8 +2,8 @@ package com.ecs.android.sample.twitter;
 
 public class Constants {
 
-	public static final String CONSUMER_KEY = "mGlNLecWYDxMnnnyEdkykw";
-	public static final String CONSUMER_SECRET= "npGimn8Tcr6o1D3Rsscaxtvuxf5SA1oYXFx7KpYxk";
+	public static final String CONSUMER_KEY = "<FILL IN YOUR CONSUMER KEY FROM TWITTER HERE>";
+	public static final String CONSUMER_SECRET= "<FILL IN YOUR CONSUMER SECRET FROM TWITTER HERE>";
 	
 	public static final String REQUEST_URL = "http://api.twitter.com/oauth/request_token";
 	public static final String ACCESS_URL = "http://api.twitter.com/oauth/access_token";
